66f48331f7146fe3f743ad8f87174b9dccd74093
Yoshi Grimbal <88178558+bruhmomentobama@users.noreply.github.com>
2023-01-24 11:52:30 -0500

    Updated the js, close to final product

--- js/hangman.js
+++ js/hangman.js
@@ -32,11 +32,81 @@ function randomWord() {
 }
 
 //Make buttons that display the alphabet, prepare for headaches, future me.
+function generateButtons() {
+  let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-dark m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
 //Detects the button that was clicked
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
+}
 //Update hangman every wrong guess
+function updateHangmanPicture() {
+  document.getElementById('hangmanPic').src = './images/' + mistakes + '.png';
+}
 //Checks to see if game is won
-//Checks to see if game is lost + insult
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    let audio = new Audio('./sound/correct.mp3');
+    audio.play();
+  }
+}
+//Checks to see if game is lost + audio
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    let audio = new Audio('./sound/mortis.mp3');
+    audio.volume = 0.3; audio.play() ;
+  }
+}
 //Shows answer
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
 //Mistake updater
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
 //Restarts the game
+function reset() {
+  mistakes = 0;
+  guessed = [];
+  document.getElementById('hangmanPic').src = './images/0.png';
+
+  randomWord();
+  guessedWord();
+  updateMistakes();
+  generateButtons();
+}
 //Maximum tries allowed
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
